59db00c37db68e56d6685a25e45e834c57281779
Sudha <sudha.sandhiya@gmail.com>
2024-12-18 09:50:02 +0530

    login response is changed to show the user name

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.post('/', (req, res) => {
   const { username, password } = req.body;
 
   if (username === 'test' && password === 'test') {
-    res.status(200).json({ message: 'Login successful' });
+    res.status(200).json({ message: 'Login ' + username +  ' successful' });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
